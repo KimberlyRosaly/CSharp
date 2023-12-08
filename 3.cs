8f60a9bb6403a6567aa325c16f59f07f04458030
KimberlyRosaly <01010110.v@gmail.com>
2023-12-07 21:42:54 -0500

    Show off how to ask for user input within a console, snippet displays text first, then requests input

---
+++ 3.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Form
+{
+  class Program
+  {
+    //  can be called without creating an instance of the Program class.
+    // method does not return any value after execution
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello! Please, input your pet's name.");
+      Console.WriteLine("Press [ENTER] key to continue.");
+      // CAMELCASE - NAMING CONVENTION FOR VARIABLES
+      string petsName = Console.ReadLine():
+    }
+  }
+}
