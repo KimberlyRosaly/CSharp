491641ac9d61409058ec96fa766d31f2cb39a20e
KimberlyRosaly <01010110.v@gmail.com>
2023-12-10 23:27:18 -0500

    Add user input for favorite number

---
+++ 4.cs
@@ -0,0 +1,19 @@
+
+namespace Form
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+{
+        // Ask user for fave number
+      Console.Write("Enter your favorite number!: ");
+      // Convert method
+      int faveNumber = Convert.ToInt32(Console.ReadLine());
+      // GET USER INPUT VIA THE CONSOLE
+      // RETURNED STRING BY USER INPUT : CONVERT INTO AN INTEGER
+      
+
+    }
+  }
+}
