66cd3393c81eb38318410e05af9d8eec3c7389e3
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2023-12-14 23:50:53 -0500

    codecademy cheat sheet

--- 5.cs
+++ 5.cs
@@ -2,3 +2,9 @@
 // prompt user input
 // capture user input and return to console
 // convert string input to another data type
+
+// LIST - sequential collection of values - hold references to any type
+List<string> citiesList = new List<string>();
+citiesList.Add("Los Angeles");
+citiesList.Add("New York City");
+citiesList.Add("Dubai");
