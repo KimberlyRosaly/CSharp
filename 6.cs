f30becae1fedf7f8312c775e286674c78a44444f
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2023-12-19 23:24:12 -0500

    a list and a for loop that prints to console with strings interpolated

--- 6.cs
+++ 6.cs
@@ -2,3 +2,17 @@
 // get input
 // change data type of input
 // iterate through menu of options
+
+// name the list of strings 'options'
+// create a new instance object containing strings
+
+List<string> options = new List<string> {"(a)", "(b)"};
+
+// initialization statement
+// expression to check : condition
+// expression to evaluate if check is true
+for (int i = 0; i < options.Count; i++)
+{
+    // print to console looped string interpolation
+    Console.WriteLine($"{options[i]}");
+}
