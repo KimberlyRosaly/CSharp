f63e992cdf219eabc0ff4d699c15d10e98d14db1
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2023-12-21 22:12:42 -0500

    file now has a program to run code block of data stored in variables, iterations that print list items to the console, and then wait for user input

--- 6.cs
+++ 6.cs
@@ -1,21 +1,34 @@
-// prompt
-// get input
-// change data type of input
-// iterate through menu of options
+using System;
 
-// name the list of strings 'options'
-// create a new instance object containing strings
-
-List<string> options = new List<string> {"(a)", "(b)"};
-
-// initialization statement
-// expression to check : condition
-// expression to evaluate if check is true
-for (int i = 0; i < options.Count; i++)
+class Program
 {
-    // print to console looped string interpolation
-    Console.WriteLine($"{options[i]}");
-}
+    static void Main()
+    {
+        // V A R I A B L E S
+        // name a list of strings 'options'
+        // create a new instance object containing strings
+        List<string> options = new List<string> {"(a)", "(b)"};
+        // create array : assign variable : contain an object of strings
+        string[] menuOptions = { "Option 1", "Option 2", "Option 3", "Exit" };
 
-// create array of strings within object
-string[] menuOptions = { "Option 1", "Option 2", "Option 3", "Exit" };
+        // P R I N T
+        // Display instructions to user
+        // print string to console
+        Console.WriteLine("Please, type in your selection and press the [ENTER] key: ");
+
+        // L O O P
+        // initialization statement
+        // expression to check : condition
+        // expression to evaluate if check returns true
+        for (int i = 0; i < options.Count; i++)
+        {
+            // print to console looped string interpolation
+            Console.WriteLine($"{options[i]}");
+        }
+
+        // C A P T U R E
+        // contain user input within a variable
+        string userInput = Console.ReadLine();
+
+    }
+}
