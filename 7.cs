38b7119d7a6774c7959b4b25e2e29fc7f3db1bda
KimberlyRosaly <01010110.v@gmail.com>
2023-12-27 23:40:16 -0500

    new snippet to learn changing values within a list

---
+++ 7.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        // CREATE LIST
+        // ASK THE USER FOR INPUT
+        // APPEND THE CAPTURED INPUT VALUE TO THE LIST
+        // PRINT UPDATED LIST
+
+    }
+}
