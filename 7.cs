6f3688a79909c3f51b12e03b4bbfb2843ccc9875
KimberlyRosaly <01010110.v@gmail.com>
2023-12-28 23:46:40 -0500

    basic sample list created, console prompts user with instructions, captures within a string variable, modifies the sample list by appending with variable value, and then prints to the console for all the world to see

--- 7.cs
+++ 7.cs
@@ -6,9 +6,15 @@ class Program
     {
 
         // CREATE LIST
+        List<string> menu = new List<string> {"one", "two", "three", "four"};
         // ASK THE USER FOR INPUT
+        Console.WriteLine("TYPE ENTRY AND SUBMIT WITH [ENTER] KEY.");
+        // CAPTURE USER INPUT
+        string capturedInput = Console.ReadLine();
         // APPEND THE CAPTURED INPUT VALUE TO THE LIST
+        menu.Add(capturedInput);
         // PRINT UPDATED LIST
+        Console.WriteLine(menu);
 
     }
 }
