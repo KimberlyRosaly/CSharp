2512008c366a3d70ea092c0d54e93aec04f060d8
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-01-01 23:52:59 -0500

    pseudocode a new snippet to handle looping, string conversion to integer, and sorting

---
+++ 8.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // OBJECTIVE : CREATE LOOPY LOGIC TO REQUEST DATA FROM USER
+
+        // (1)
+        //  PROMPT USER : INSTRUCTIONS TO ENTER NUMBER
+        // AWAIT AND CAPTURE USER RESPONSE
+        // (2)
+        // PROMPT USER TO ENTER ANOTHER NUMBER
+        // AWAIT AND CAPTURE
+        // (3)
+        // PROMPT USER TO ENTER ANOTHER NUMBER
+        // AWAIT AND CAPTURE
+
+        // CREATE LIST WITH RESPONSE
+        // CONVERT LIST STRINGS TO INTEGERS
+        // SORT VALUES WITHIN LIST
+        // PRINT SORTED LIST TO CONSOLE        
+
+    }
+}
