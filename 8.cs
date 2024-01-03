cc0fcb6ba68ad2f267e01cf6c49a005b503aa6ed
VQuine <40582813+VQuine@users.noreply.github.com>
2024-01-02 23:58:42 -0500

    create loop to display text and interpolated value to console

--- 8.cs
+++ 8.cs
@@ -6,15 +6,16 @@ class Program
     {
         // OBJECTIVE : CREATE LOOPY LOGIC TO REQUEST DATA FROM USER
 
-        // (1)
-        //  PROMPT USER : INSTRUCTIONS TO ENTER NUMBER
-        // AWAIT AND CAPTURE USER RESPONSE
-        // (2)
-        // PROMPT USER TO ENTER ANOTHER NUMBER
-        // AWAIT AND CAPTURE
-        // (3)
-        // PROMPT USER TO ENTER ANOTHER NUMBER
-        // AWAIT AND CAPTURE
+        // [x] (1), (2), (3) | [x] PROMPT USER INSTRUCTIONS | [] AWAIT AND CAPTURE USER RESPONSE
+
+        // 'FOR' LOOP : initialization statement, initial value : integer variable declaration 
+        // (XD)micro if statement : if 'i' is 'less than' '3' : iteration statement to execute
+        // post-increment operation : 'i' by '1' after each pass through the loop
+        for (int i = 0; i < 3; i++)
+        {
+            // string interpolation signaled with: $"{variableName}"
+            Console.WriteLine($"ENTER NUMERICAL VALUE <No. {i+1} of 3> AND PRESS [ENTER]");
+        }
 
         // CREATE LIST WITH RESPONSE
         // CONVERT LIST STRINGS TO INTEGERS
