506f1630791bbaeff920e11de7049660763e5479
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-01-09 22:42:16 -0500

    fix up some code smell, changing logic a bit to refactored versions of blocks to more directly handle the string input,  convert to integer to add it to my list (or error out), sort, then seperate with commas, placing the sorted integers back into a string to print onto the console

--- 8.cs
+++ 8.cs
@@ -6,12 +6,13 @@ class Program
     {
         // OBJECTIVE : CREATE LOOPY LOGIC TO REQUEST DATA FROM USER
 
-        // [x] (1), (2), (3) | [x] PROMPT USER INSTRUCTIONS | [x] AWAIT AND CAPTURE USER RESPONSE
+       
 
         // - - - - - - - - - - - - - - L I S T   O F   N U M B E R S
         // EMPTY, INITIALIZED LIST
-        List<string> numericalList = new List<string> {};
+        List<int> numericalList = new List<int> {};
 
+        // - - - - - - - - - - - - - - l O O P  3  T I M E S - - - -
         // 'FOR' LOOP : initialization statement, initial value : integer variable declaration 
         // (XD)micro if statement : if 'i' is 'less than' '3' : iteration statement to execute
         // post-increment operation : 'i' by '1' after each pass through the loop
@@ -19,38 +20,25 @@ class Program
         {
             // string interpolation signaled with: $"{variableName}" syntax
             Console.WriteLine($"ENTER NUMERICAL VALUE <No. {i+1} of 3> AND PRESS [ENTER]");
-
+            // capture user input as a string
             string userInput = Console.ReadLine();
-            numericalList.Add(userInput);
-        }
 
-        
-        List<int> numericalListOfIntegers = new List<int>();
-        foreach (string str in numericalList)
-        {
-            if(int.TryParse(str, out int number))
+            if(int.TryParse(userInput, out int number))
             {
-                numericalListOfIntegers.Add(number);
+                numericalList.Add(number);
             }
             else
             {
-                Console.WriteLine("UH OH!")
+                Console.WriteLine("ERROR : VALUE IS NOT AN INTEGER : REQUESTS FOR INPUT HAVE BEEN TERMINATED")
             }
-        }
+        }        
         
         // SORT VALUES WITHIN LIST
-        numericalListOfIntegers.Sort();
-        // PRINT SORTED LIST TO CONSOLE        
+        numericalList.Sort();
 
-        // Console.WriteLine(numericalListOfIntegers); // WILL PRINT OUT TYPE OF OBJECT
 
-        // ITERATE OVER EACH ITEM IN THE LIST AND PRINT
-        // foreach (int num in numericalListOfIntegers)
-        // {
-        //     Console.WriteLine(num);
-        // }
-
-        Console.WriteLine(string.Join(", ", numericalListOfIntegers));
+        // PRINT SORTED LIST TO CONSOLE
+        Console.WriteLine(string.Join(", ", numericalList));
 
     }
 }
