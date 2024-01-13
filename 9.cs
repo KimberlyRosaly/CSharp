ea9477adf3784e07b5b94271e836b2382185af0d
VQuine <40582813+VQuine@users.noreply.github.com>
2024-01-12 23:36:20 -0500

    Create a new snippet and pseudocode through objective

---
+++ 9.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // OBJECTIVE : CREATE MENU TO REMOVE OR ADD TO VALUES
+        // CREATE LIST OF EXAMPLE DATA VALUES
+        // PRINT LIST TO CONSOLE
+        // PRINT MENU OF ACTIONS
+            // ADD VALUE TO LIST
+            // REMOVE VALUE FROM LIST
+        // AWAIT USER INPUT
+        // PERFORM REQUESTED ACTION
+        // PRINT MODIFIED LIST TO CONSOLE
+        // PRINT MENU OF ACTIONS
+
+    }
+}
