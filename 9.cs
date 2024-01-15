0bd33bc2f526e944740f8ad527e00bc276e06f6d
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-01-14 22:28:50 -0500

    Add some basic code to flesh out pseudo steps

--- 9.cs
+++ 9.cs
@@ -6,11 +6,14 @@ class Program
     {
         // OBJECTIVE : CREATE MENU TO REMOVE OR ADD TO VALUES
         // CREATE LIST OF EXAMPLE DATA VALUES
+        List<str> sampleData = new List<str>();
         // PRINT LIST TO CONSOLE
+        Console.WriteLine(sampleData);
         // PRINT MENU OF ACTIONS
             // ADD VALUE TO LIST
             // REMOVE VALUE FROM LIST
         // AWAIT USER INPUT
+        string userInput = Console.ReadLine();
         // PERFORM REQUESTED ACTION
         // PRINT MODIFIED LIST TO CONSOLE
         // PRINT MENU OF ACTIONS
