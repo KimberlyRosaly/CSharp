3eb2adcc01309fdbe3463c37a731c81640278f90
KimberlyRosaly <01010110.v@gmail.com>
2024-01-15 23:55:27 -0500

    create one liner solutions that need logic for each step with the user

--- 9.cs
+++ 9.cs
@@ -10,13 +10,18 @@ class Program
         // PRINT LIST TO CONSOLE
         Console.WriteLine(sampleData);
         // PRINT MENU OF ACTIONS
+        Console.WriteLine(menu);
             // ADD VALUE TO LIST
+            sampleData.Add(selection);
             // REMOVE VALUE FROM LIST
+            sampleData.Remove(selection);
         // AWAIT USER INPUT
         string userInput = Console.ReadLine();
         // PERFORM REQUESTED ACTION
         // PRINT MODIFIED LIST TO CONSOLE
+        Console.WriteLine(menu);
         // PRINT MENU OF ACTIONS
+        Console.WriteLine(menu);
 
     }
 }
