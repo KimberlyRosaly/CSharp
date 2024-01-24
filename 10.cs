b836b8d367bac53bf4aa26c41bd53ad01a34389a
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-01-23 23:49:36 -0500

    create new snippet pseudocode to encapsulate a simpler logic stream to simply add a value to a list

---
+++ 10.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // =====================================================
+        // OBJECTIVE : CREATE MENU TO ADD TO VALUES
+        // =====================================================
+        /*
+            CREATE LIST OF STRINGS CONTAINING PROMPTS
+            CREATE LIST OF SAMPLE DATA VALUES OF STRING
+
+            PROMPT[0] = INSTRUCTIONS TO USER
+            
+            DISPLAY DATA
+            ASK IF USER WOULD LIKE TO ADD A VALUE
+            IF USER SAYS [YES]
+                ASK USER FOR VALUE TO ADD AND STORE IT
+                ADD NEW VALUE TO LIST
+                PRINT NEW LIST TO MENU
+                LOOP BACK INTO ASKING THE USER IF THEY WANT TO ADD A VALUE
+            IF USER SAYS [NO]
+                SAY GOODBYE TO USER
+                TERMINATE PROGRAM
+        */
+    }
+}
