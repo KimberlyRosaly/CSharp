4543a0f9008543e77668c43b10f788612577c1b0
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-01-24 23:37:19 -0500

    Get some lists initialized with string data

--- 10.cs
+++ 10.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
@@ -7,22 +8,25 @@ class Program
         // =====================================================
         // OBJECTIVE : CREATE MENU TO ADD TO VALUES
         // =====================================================
-        /*
-            CREATE LIST OF STRINGS CONTAINING PROMPTS
-            CREATE LIST OF SAMPLE DATA VALUES OF STRING
+        
+            // CREATE LIST OF STRINGS CONTAINING PROMPTS
+            List<strings> prompts = new List<strings> {"INTRODUCTION", "INSTRUCTIONS"};
 
-            PROMPT[0] = INSTRUCTIONS TO USER
+            // CREATE LIST OF SAMPLE DATA VALUES OF STRING
+            List<strings> sampleData = new List<strings> {"A", "B", "D"};
+
+            // PROMPT[0] = INSTRUCTIONS TO USER
             
-            DISPLAY DATA
-            ASK IF USER WOULD LIKE TO ADD A VALUE
-            IF USER SAYS [YES]
-                ASK USER FOR VALUE TO ADD AND STORE IT
-                ADD NEW VALUE TO LIST
-                PRINT NEW LIST TO MENU
-                LOOP BACK INTO ASKING THE USER IF THEY WANT TO ADD A VALUE
-            IF USER SAYS [NO]
-                SAY GOODBYE TO USER
-                TERMINATE PROGRAM
-        */
+            // DISPLAY DATA
+            // ASK IF USER WOULD LIKE TO ADD A VALUE
+            // IF USER SAYS [YES]
+                // ASK USER FOR VALUE TO ADD AND STORE IT
+                // ADD NEW VALUE TO LIST
+                // PRINT NEW LIST TO MENU
+                // LOOP BACK INTO ASKING THE USER IF THEY WANT TO ADD A VALUE
+            // IF USER SAYS [NO]
+                // SAY GOODBYE TO USER
+                // TERMINATE PROGRAM
+        
     }
 }
