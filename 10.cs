4c4ed1f3054dda7c362bcf393414936930b0cb5b
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-01-25 23:58:24 -0500

    follow the psuedocode and write in code lines of functions, getting started on an IF STATEMENT

--- 10.cs
+++ 10.cs
@@ -10,18 +10,29 @@ class Program
         // =====================================================
         
             // CREATE LIST OF STRINGS CONTAINING PROMPTS
-            List<strings> prompts = new List<strings> {"INTRODUCTION", "INSTRUCTIONS"};
+            List<strings> prompts = new List<strings> {"INTRODUCTION", "QUERY", "INSTRUCTIONS"};
 
             // CREATE LIST OF SAMPLE DATA VALUES OF STRING
             List<strings> sampleData = new List<strings> {"A", "B", "D"};
 
-            // PROMPT[0] = INSTRUCTIONS TO USER
-            
+            // TELL USER ABOUT PROGRAM
+            Console.WriteLine(prompts[0]);
             // DISPLAY DATA
+            Console.WriteLine(string.join(" | ", sampleData));
             // ASK IF USER WOULD LIKE TO ADD A VALUE
+            Console.WriteLine(prompts[1]);
+            // GET USER INPUT AND STORE WITHIN VARIABLE
+            string userInputConfirmation = Console.ReadLine();
             // IF USER SAYS [YES]
-                // ASK USER FOR VALUE TO ADD AND STORE IT
+            if (userInputConfirmation == "YES")
+            {
+                //  INSTRUCT USER TO INPUT VALUE
+                Console.WriteLine(prompts[2]);
+                //  STORE USER RESPONSE WITHIN A VARIABLE
+                string userInputValue = Console.Readline();
                 // ADD NEW VALUE TO LIST
+                sampleData.Add(userInputValue);
+            }
                 // PRINT NEW LIST TO MENU
                 // LOOP BACK INTO ASKING THE USER IF THEY WANT TO ADD A VALUE
             // IF USER SAYS [NO]
