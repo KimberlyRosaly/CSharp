26e77a8d7ed7c9186a64340d77680377e493a68d
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-01-27 23:49:19 -0500

    make some syntax adjustments > incorrect casing

--- 10.cs
+++ 10.cs
@@ -10,15 +10,15 @@ class Program
         // =====================================================
         
             // CREATE LIST OF STRINGS CONTAINING PROMPTS
-            List<strings> prompts = new List<strings> {"INTRODUCTION", "QUERY", "INSTRUCTIONS", "OUTRO"};
+            List<string> prompts = new List<string> {"INTRODUCTION", "QUERY", "INSTRUCTIONS", "OUTRO"};
 
             // CREATE LIST OF SAMPLE DATA VALUES OF STRING
-            List<strings> sampleData = new List<strings> {"A", "B", "D"};
+            List<string> sampleData = new List<string> {"A", "B", "D"};
 
             // TELL USER ABOUT PROGRAM
             Console.WriteLine(prompts[0]);
             // DISPLAY DATA
-            Console.WriteLine(string.join(" | ", sampleData));
+            Console.WriteLine(string.Join(" | ", sampleData));
             // ASK IF USER WOULD LIKE TO ADD A VALUE
             Console.WriteLine(prompts[1]);
             // GET USER INPUT AND STORE WITHIN VARIABLE
@@ -29,11 +29,11 @@ class Program
                 //  INSTRUCT USER TO INPUT VALUE
                 Console.WriteLine(prompts[2]);
                 //  STORE USER RESPONSE WITHIN A VARIABLE
-                string userInputValue = Console.Readline();
+                string userInputValue = Console.ReadLine();
                 // ADD NEW VALUE TO LIST
                 sampleData.Add(userInputValue);
                 // PRINT NEW LIST TO CONSOLE
-                Console.WriteLine(string.join(" | ", sampleData));
+                Console.WriteLine(string.Join(" | ", sampleData));
             }
             // IF USER SAYS [NO]
             else if (userInputConfirmation == "NO")
