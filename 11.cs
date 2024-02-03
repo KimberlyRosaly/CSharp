f287ac7b12eb547f068d3136352d5ffe435b512f
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-02-02 23:20:37 -0500

    create a new snippet to make use of searching for matching data with numerical values prepended, write out the pseudocode

---
+++ 11.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // =====================================================
+        // OBJECTIVE : CREATE LIST OF VALUES, 
+        //              A WORD AND ITS DEFINITION
+        //            USER SELECTS A WORD
+        //              AND IS THEN SHOWN IT'S DEFINITION
+        // =====================================================
+        // CREATE OBJECT WITH 2 VALUES {"WORD", "DEFINITION"}
+        // CREATE ANOTHER OBJECT WITH 2 VALUES {"WORD2", "DEFINITION2"}
+        // DISPLAY "1. WORD" | "2. WORD"
+        // USER INPUTS NUMERICAL VALUE
+        // FIND MATCHING OBJECT 
+        // DISPLAY MATCHING OBJECT "WORD - DEFINITION" | "WORD2 - DEFINITION2"
+           
+        
+    }
+}
