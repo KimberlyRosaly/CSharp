13145b15303ca20c95df9adea60a2626704d4203
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-02-19 23:10:39 -0500

    create a new dictionary to house just the error messages alone, single responsibility principle, code is clearer to read by all humans

--- 11.cs
+++ 11.cs
@@ -22,6 +22,7 @@ class Program
         List<string> words = new List<string>();
         Dictionary<string, string> dictionary = new Dictionary<string, string>();
         Dictionary<string, string> prompts = new Dictionary<string, string>();
+        Dictionary<string, string> errors = new Dictionary<string, string>();
         // INSERT SAMPLE DATA VALUES
         words.Add("word");
         words.Add("word2");
@@ -29,8 +30,8 @@ class Program
         dictionary.Add("word2", "definition2");
         prompts.Add("welcome", "USER WELCOME MESSAGE");
         prompts.Add("instructions", "USER, PLEASE ENTER INTEGER AND PRESS [ENTER]");
-        prompts.Add("error - not an integer", "ERROR - INVALID ENTRY - NOT A NUMBER - PLEASE TRY AGAIN");
-        prompts.Add("error - not in range", "ERROR - INVALID ENTRY - ENTRY NOT FOUND - PLEASE TRY AGAIN");
+        errors.Add("not an integer", "ERROR - INVALID ENTRY - NOT A NUMBER - PLEASE TRY AGAIN");
+        errors.Add("not in range", "ERROR - INVALID ENTRY - ENTRY NOT FOUND - PLEASE TRY AGAIN");
         // =====================================================
         // =======================================DISPLAY VALUES
         int index = 1;
@@ -49,11 +50,11 @@ class Program
             string selection = Console.ReadLine();
             if (!int.TryParse(selection, out selectionToInteger))
             {
-                Console.WriteLine(prompts["error - not an integer"]);
+                Console.WriteLine(errors["not an integer"]);
             }
             else if (selectionToInteger < 1 || selectionToInteger > words.Count)
             {
-                Console.WriteLine(prompts["error - not in range"]);
+                Console.WriteLine(errors["not in range"]);
             }
             else
             {
