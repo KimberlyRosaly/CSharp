b83dc6c15edaebcaefb71b9e9a5c50f8bc2e08f4
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-02-20 21:35:06 -0500

    create a new namespace that is blank to contain a new sample program

--- 12.cs
+++ 12.cs
@@ -1,3 +1,14 @@
-// utilize searching through different datatypes
-// utilize conversion of datatypes
-// string to integer and back to string again
+using System;
+
+// KEYWORD - USED TO DEFINE A NAMESPACE
+// NAMESPACE : PROVIDES SCOPE FOR IDENTIFIERS WITHIN : USED TO ORGANIZE CODE INTO LOGICAL GROUPS
+namespace NamespaceExample // ANY VALID IDENTIFIER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // CODE TO EXECUTE
+        }
+    }
+}
