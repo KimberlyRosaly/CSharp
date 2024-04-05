64b63c50509e0f38a769337d32e85d75def75879
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-04-05 19:48:23 -0400

    display a message to the user on the console on it's own thread

--- 13.cs
+++ 13.cs
@@ -1,41 +1,4 @@
-// CREATE SOMETHING GAME-LIKE AND FUN
-// CONSIDER HOW TO MAKE SOMETHING SIMPLE ALONG THE LINES OF A PET SIMULATOR
-// CONSIDER HOW TO USE ASYNC AWAIT
-    // PET NEEDS TO EAT AND YOU HAVE TO COOK A FOOD ITEM
-    // IF THE USER WAITS TOO LONG TO COOK THE FOOD ITEM - IT BURNS
-    // IF THE USER WAITS NOT LONG ENOUGH- THE FOOD ITEM IS RAW AND UNEDIBLE
-    // IF THE USER WAITS A SPECIFIED AMOUNT OF TIME - THE FOOD IS COOKED JUST RIGHT AND CAN BE FED 
-
-// PET APPEARS TO BREATHE
-// PET FRAME 1 (SMALLER)
-// PET FRAME 2 (LARGER)
-// TIMER IS UTILIZED & LOOP
-// DISPLAY FRAME 1 UPON PAGE LOAD
-// WAIT 1 SECOND
-// CLEAR SCREEN
-// DISPLAY FRAME 2
-// WAIT 1 SECOND
-// CLEAR SCREEN
-// DISPLAY FRAME 1
-// LOOP INFINITELY
-
-// DISPLAY HUNGER GAUGE WITH 10 EMPTY 1-SECOND BLOCKS
-// INITIALIZE TIMER
-// EVERY 1 SECOND, ADD A FILLED-IN BLOCK
-// AFTER 10 SECONDS, GAUGE IS FILLED
-// WARN USER THAT PET IS HUNGRY AND NEEDS TO EAT 
-// IF PET IS NOT FED WITHIN 10 SECONDS
-    // PET TURNS INTO SKELETON
-       // CLEAR SCREEN
-       // DISPLAY SKELETON FRAME 
-       // MAKE SKELETON BREATHE
-    // ELSE LIVING PET CONTINUES TO BREATHE
-
-//! + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +
-// 'using' > KEYWORD > DIRECTIVE TO IMPORT NAMESPACES > GROUPS OF RELATED 'TYPES'
-// 'System' > ROOT NAMESPACE > FUNDAMENTAL & BASE CLASSES
 using System;
-using System.Timers;
 // ALLOW MULTITHREADING
 using System.Threading.Tasks;
 
@@ -53,30 +16,29 @@ class Program
 |  -   -  |
  \_ _ _ _/
 ";
-    // 'static' > KEYWORD FOR BELONGING TO THE 'Program' CLASS ITSELF AND NOT INSTANCE
-    // 'void' > RETURN TYPE > SIGNIFY THAT METHOD DOES NOT RETURN A VALUE
-    // 'main' > CONVENTIONAL METHOD NAME > 1ST TO GET CALLED > APPLICATION ENTRY POINT
-    // '(string[] args)' > METHOD PARAMETERS PASSED IN WHEN APPLICATION STARTS 
-    //      TAKE IN AN ARRAY OF STRINGS OF COMMAND-LINE ARGUMENTS
+
     static void Main(string[] args)
     {
         // ENCAPSULATED WILL RUN ON ITS OWN THREAD > SEPERATE AND ABLE TO RUN CONCURRENTLY
         Task.Run(() =>
-        // INITIALIZE A TIMER TO BE TRIGGERED EVERY 1 SECOND
-        // CLASS | STORED REFERENCE | INITIALIZATION OF INSTANCE | ARGUMENT FOR CONSTRUCTOR
-        Timer timer = new Timer(1000);
-        // TOGGLE FRAMES WITH BOOLEAN AND SET IT'S DEFAULT VALUE TO BE FALSEY
-        bool toggle = false;
-
-        // SET TO ADD > THEN REDEFINE > PASS IN EVENT HANDLER
-        timer.Elapsed += (sender, e) =>
         {
-            // CLEAR CONSOLE FOR A BLANK SLATE TO WORK WITH
-            Console.Clear();
-            // TERNARY - IF TOGGLE IS TRUTHY, DISPLAY LARGER FRAME, OTHERWISE DISPLAY SMALLER FRAME
-            Console.WriteLine(toggle ? petInhale : petExhale);
-            // REDEFINE VARIABLE TO THE OPPOSITE OF WHAT IT CURRENTLY IS
-            toggle = !toggle;
+            // INITIALIZE A TIMER TO BE TRIGGERED EVERY 1 SECOND
+            // CLASS | STORED REFERENCE | INITIALIZATION OF INSTANCE | ARGUMENT FOR CONSTRUCTOR
+            System.Timers.Timer timer = new System.Timers.Timer(1000);
+            // TOGGLE FRAMES WITH BOOLEAN AND SET IT'S DEFAULT VALUE TO BE FALSEY
+            bool toggle = false;
+
+            // SET TO ADD > THEN REDEFINE > PASS IN EVENT HANDLER
+            timer.Elapsed += (sender, e) =>
+            {
+                // CLEAR CONSOLE FOR A BLANK SLATE TO WORK WITH
+                Console.Clear();
+                // TERNARY - IF TOGGLE IS TRUTHY, DISPLAY LARGER FRAME, OTHERWISE DISPLAY SMALLER FRAME
+                Console.WriteLine(toggle ? petInhale : petExhale);
+                // REDEFINE VARIABLE TO THE OPPOSITE OF WHAT IT CURRENTLY IS
+                toggle = !toggle;
+            };
+
             // BEGIN THE TIMER
             timer.Start();
             // TIMER RUNS INFINITELY
@@ -84,9 +46,5 @@ class Program
         });
 
         Console.WriteLine("HELLO, USER!");
-
-
     }
 }
-//! + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +
-
