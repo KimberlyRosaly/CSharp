07c4568ee9988fcb186333f0549157f6d366a4b2
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-04-08 22:53:01 -0400

    split the program up into 3 threads that will run concurrently, their own rhythms, the first to display text, the second to show off the hunger meter, and the third to print out my breathing pet

--- 13.cs
+++ 13.cs
@@ -16,10 +16,26 @@ class Program
 |  -   -  |
  \_ _ _ _/
 ";
+    static string hungerMeter = "[][][][][]";
 
     static void Main(string[] args)
     {
-        // ENCAPSULATED WILL RUN ON ITS OWN THREAD > SEPERATE AND ABLE TO RUN CONCURRENTLY
+        // (1) PROMPTS : ENCAPSULATED WILL RUN ON ITS OWN THREAD > SEPERATE AND ABLE TO RUN CONCURRENTLY
+        Task.Run(() =>
+        {
+                Console.WriteLine("WELCOME HOME!);
+                Console.WriteLine("MY NAME IS RIXIN!");
+                Console.WriteLine("Please, make sure I dont get too hungry!"
+        }
+        );
+        // (2) HUNGER : ENCAPSULATED WILL RUN ON ITS OWN THREAD > SEPERATE AND ABLE TO RUN CONCURRENTLY
+        Task.Run(() =>
+        {
+                Console.WriteLine(hungerMeter);
+        }
+        );
+
+        // (3) PET : ENCAPSULATED WILL RUN ON ITS OWN THREAD > SEPERATE AND ABLE TO RUN CONCURRENTLY
         Task.Run(() =>
         {
             // INITIALIZE A TIMER TO BE TRIGGERED EVERY 1 SECOND
@@ -33,10 +49,6 @@ class Program
             {
                 // CLEAR CONSOLE FOR A BLANK SLATE TO WORK WITH
                 Console.Clear();
-                Console.WriteLine("WELCOME HOME!);
-                Console.WriteLine("MY NAME IS RIXIN!");
-                Console.WriteLine("Please, make sure I dont get too hungry!"
-                Console.WriteLine(hungerMeter);
                 // TERNARY - IF TOGGLE IS TRUTHY, DISPLAY LARGER FRAME, OTHERWISE DISPLAY SMALLER FRAME
                 Console.WriteLine(toggle ? petInhale : petExhale);
                 // REDEFINE VARIABLE TO THE OPPOSITE OF WHAT IT CURRENTLY IS
