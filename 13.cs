b7b7a2859cce0119edd062366d40fd0bfea64f03
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-04-10 23:20:56 -0400

    add some additional hunger meters to display and make adjustments for formatting so the pet, prompts, and meter do not overlap

--- 13.cs
+++ 13.cs
@@ -69,3 +69,91 @@ class Program
 
     }
 }
+using System;
+// ALLOW MULTITHREADING
+using System.Threading.Tasks;
+
+class Program
+{
+    static string petInhale = @"
+   _ _ _ 
+ /       \
+ | -   - | 
+ \_ _ _ _/
+";
+    static string petExhale = @"
+   _ _ _ 
+ /       \
+|  -   -  |
+ \_ _ _ _/
+";    static string petDead = @"
+   _ _ _ 
+ /       \
+|  x   X  |
+ \_ _ _ _/
+";
+    static string hungerMeter0 = "[ ][ ][ ][ ][ ]";
+    //static string hungerMeter1 = "[x][ ][ ][ ][ ]";
+    //static string hungerMeter2 = "[x][x][ ][ ][ ]";
+    //static string hungerMeter3 = "[x][x][x][ ][ ]";
+    //static string hungerMeter4 = "[x][x][x][x][ ]";
+    //static string hungerMeter5 = "[x][x][x][x][x]";
+
+    static object consoleLock = new object();
+
+    static void Main(string[] args)
+    {
+        // (1) PROMPTS
+        lock (consoleLock)
+        {
+            Console.SetCursorPosition(0, 2);
+            Console.WriteLine("WELCOME HOME!");
+            Console.WriteLine("I AM YOUR PET!");
+            Console.WriteLine("Please, make sure I dont get too hungry!");
+        }
+
+        // (2) HUNGER
+        Task.Run(async () =>
+        {
+            while (true)
+            {
+                lock (consoleLock)
+                {
+                    Console.SetCursorPosition(0, 6);
+                    Console.Write(hungerMeter0);
+                }
+                await Task.Delay(1000);
+            }
+        });
+
+        // (3) PET
+        Task.Run(() =>
+        {
+            System.Timers.Timer timer = new System.Timers.Timer(1000);
+            bool alive = true;
+            bool breathing = false;
+
+            timer.Elapsed += (sender, e) =>
+            {
+                lock (consoleLock)
+                {
+                    if (!alive)
+                    {
+                        Console.SetCursorPosition(0, 7);
+                        Console.Write(petDead);
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(0, 7);
+                        Console.Write(breathing ? petInhale : petExhale);
+                    };
+                };
+                breathing = !breathing;
+            };
+
+            timer.Start();
+        });
+
+        Console.ReadLine();
+    }
+}
