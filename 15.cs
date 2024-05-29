a54d764f8820f71b80e3b3b168862db65c540f2f
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-05-28 22:50:50 -0400

    add some dialog to confirm the user's email address and desired username, and some pseudocode to make a loop for corrections they may want to make before adding the entry to the dictionary

--- 15.cs
+++ 15.cs
@@ -31,8 +31,21 @@ class Program
             // INSTRUCT USER TO ENTER AN EMAIL ADDRESS TO ASSOCIATE WITH THEIR NEW USERNAME
             Console.WriteLine("Welcome, new user. Please, type in an email address to associate with your new account and press [ENTER].")
             string emailAddress = Console.ReadLine();
+        }
+        // CONFIRM USERNAME AND EMAIL ADDRESS WITH THE USER
+        Console.WriteLine($"Please, verify your username and email address: {username} - {emailAddress}");
+        Console.WriteLine("If this is correct, press [1] and the [ENTER] key. If you need to make a correction, press [2] and press [ENTER].")
+        string confirmation = Console.ReadLine();
+        if (confirmation == 1)
+        {
             // CREATE NEW USERNAME ENTRY WITH ITS EMAIL ADDRESS
             accounts.Add(username, emailAddress);
+        }
+        else
+        {
+            // ASK USER TO INPUT A USERNAME AND EMAIL ADDRES
+            // GRAB ONTO VALUES
+            // CONFIRM AGAIN - MAKE A LOOP SOMEWHERE
         }
         
     }
