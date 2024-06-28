72ea0f1ee9134d446cccd83cde9119d906f8d1bb
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-06-28 18:30:29 -0400

    locate API to use with no key required that can grab weather information

--- 16.cs
+++ 16.cs
@@ -1,5 +1,9 @@
 // EXPLORE ASYCHRONOUS PROGRAMMING
 // USE AN API 
+/*
+https://api.open-meteo.com/v1/gfs?latitude=40.7251&longitude=-73.2454&current=temperature_2m,rain,wind_speed_10m,wind_direction_10m,wind_gusts_10m&daily=sunrise,sunset&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&forecast_days=1
+OPEN-METEO FORECAST API | OPEN-SOURCE - FREE
+*/
 // CREATE A WINDOW INSTEAD OF PRINTING TO THE CONSOLE
 // SHOW THE CURRENT WEATHER IN REAL-TIME
 // -TEMPERATURE 
