0688626b1ed4a91948697e85a685621618508831
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-07-07 21:12:55 -0400

    store the forecast API into a variable and pass it to the the HTTP Get request function

--- 16.cs
+++ 16.cs
@@ -2,10 +2,9 @@ using Newtonsoft.Json; // JSON FRAMEWORK | USED FOR WEB API DEV
 
 // EXPLORE ASYCHRONOUS PROGRAMMING
 // USE AN API 
-/*
-https://api.open-meteo.com/v1/gfs?latitude=40.7251&longitude=-73.2454&current=temperature_2m,rain,wind_speed_10m,wind_direction_10m,wind_gusts_10m&daily=sunrise,sunset&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&forecast_days=1
-OPEN-METEO FORECAST API | OPEN-SOURCE - FREE
-*/
+var forecastAPIURL = "https://api.open-meteo.com/v1/gfs?latitude=40.7251&longitude=-73.2454&current=temperature_2m,rain,wind_speed_10m,wind_direction_10m,wind_gusts_10m&daily=sunrise,sunset&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&forecast_days=1"
+// OPEN-METEO FORECAST API | OPEN-SOURCE - FREE
+
 // CREATE A WINDOW INSTEAD OF PRINTING TO THE CONSOLE
 // SHOW THE CURRENT WEATHER IN REAL-TIME
 // -TEMPERATURE 
@@ -19,7 +18,7 @@ OPEN-METEO FORECAST API | OPEN-SOURCE - FREE
 // ====================EXPLORE API SNIPPET
 var client = new HttpClient();
 
-var request = new HttpRequestMessage(HttpMethod.Get, "API URL HERE");
+var request = new HttpRequestMessage(HttpMethod.Get, forecastAPIURL);
 
 var response = await client.SendAsync(request);
 response.EnsureSuccessStatusCode(); // Throw an exception if error
