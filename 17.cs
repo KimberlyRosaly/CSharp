336f06eb523b4f988f77e3074a75c751956014cf
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-07-11 22:53:44 -0400

    Create a new program to gamify dialog with the user. Pseudocode the ideas

---
+++ 17.cs
@@ -0,0 +1,10 @@
+// Create a dialog with a user
+// Show the user cute ASCII artwork for each prompt
+// Be as artistic as possible and conversational
+
+// Greet the user
+// Ask the user their name
+// Store the user's name and refer back to them in each prompt
+
+// Create a bit of a roleplaying scenario
+// Test out some gaming possibilities / concepts
