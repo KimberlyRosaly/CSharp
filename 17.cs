2922dff5f5231345dbd9b627bc2ce494c3b1730c
KimberlyRosaly <40582813+KimberlyRosaly@users.noreply.github.com>
2024-07-12 23:23:09 -0400

    flesh out the gaming concept with comments to guide

--- 17.cs
+++ 17.cs
@@ -8,3 +8,16 @@
 
 // Create a bit of a roleplaying scenario
 // Test out some gaming possibilities / concepts
+
+
+// RP CONCEPT - grow a garden
+// Be able to keep track of days that the user visits - grab onto the current date and time
+// Each day that the user uses the application, increase the growth of a plant
+// Day 1: seed - user plants the seed
+// Day 2: seedling - seed has germinated and begins to sprout
+// Day 3: bud - a bit of a vine appears with a small leaf
+// Day 4: stem & leaves - plant is able to stand and has several leaves appearing
+// Day 5: full fledged plant
+
+// IDEA : HORROR GAME - PLANT SPROUTS STRANGE EXTREMETIES - EYEBALLS, MOUTHS, TEETH, CLAWS, DEVIL TAILS
+// STRETCH GOAL : EXTREMETIES MOVE - EYEBALLS BLINK, MOUTHS SMILE, TEETH GLEAMS, CLAWS WAVE, TAILS WAG
